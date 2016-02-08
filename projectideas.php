7e6dad33f9459e64508db63be1fa5fa6bbfba9ba
Dravid Joseph <dravidjoseph@gmail.com>
2016-02-07 22:37:43 -0600

    Homework 4 Update

---
+++ projectideas.php
@@ -0,0 +1,20 @@
+
+
+<html>
+
+<body>
+	
+	<?php echo "Project Ideas"?>
+	
+	$ideas = array('Interactive Game','Mathematical Modeling');
+	
+	echo '<ul>'
+	foreach($ideas as $i){
+		echo'<li>'.$i.'</li>;
+	}
+	echo '<ul>'
+	
+</body>
+
+
+</html>
