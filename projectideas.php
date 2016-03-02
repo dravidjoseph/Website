2d9c5d530f42c1f5f4a382628a9d98db73bffa00
Dravid Joseph <dravidjoseph@gmail.com>
2016-03-02 13:01:35 -0600

    Add team members php

--- projectideas.php
+++ projectideas.php
@@ -14,8 +14,36 @@ echo '<body>';
 	echo'<li>'.Get_djosephProjectIdea(2).'</li>';
 	echo '</ul>';
 	
-	echo '<h2>Michael Wang Ideas</h2>';
-	echo 'file_get_contents("http://people.eecs.ku.edu/~mwang/projectideafunctions.php?index=0")';
+	$mwang0 = file_get_contents("http://people.eecs.ku.edu/~mwang/projectideafunctions.php?index=0");
+	$mwang1 = file_get_contents("http://people.eecs.ku.edu/~mwang/projectideafunctions.php?index=1");
+	$mwang2 = file_get_contents("http://people.eecs.ku.edu/~mwang/projectideafunctions.php?index=2");
+	$mwang3 = file_get_contents("http://people.eecs.ku.edu/~mwang/projectideafunctions.php?index=3");
+	
+	echo '<h2>Michael Wang Project Ideas</h2>';
+	echo '<ul>';
+	echo'<li>'.$mwang0.'</li>';
+	echo'<li>'.$mwang1.'</li>';
+	echo'<li>'.$mwang2.'</li>';
+	echo'<li>'.$mwang3.'</li>';
+	echo '</ul>';
+	
+	$ayeasin0 = file_get_contents("http://people.eecs.ku.edu/~tyeasin/projectideafunctions.php?index=0");
+	$ayeasin1 = file_get_contents("http://people.eecs.ku.edu/~tyeasin/projectideafunctions.php?index=1");
+
+	echo '<h2>Amber Yeasin Project Ideas</h2>';
+	echo '<ul>';
+	echo'<li>'.$ayeasin0.'</li>';
+	echo'<li>'.$ayeasin1.'</li>';
+	echo '</ul>';
+	
+	$awang0 = file_get_contents("http://people.eecs.ku.edu/~tyeasin/projectideafunctions.php?index=0");
+	$awang1 = file_get_contents("http://people.eecs.ku.edu/~tyeasin/projectideafunctions.php?index=1");
+	
+	echo '<h2>Alan Wang Project Ideas</h2>';
+	echo '<ul>';
+	echo'<li>'.$awang0.'</li>';
+	echo'<li>'.$awang1.'</li>';
+	echo '</ul>';
 	
 	
 echo '</body>';
