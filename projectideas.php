566f5616acbe670acdb203a800d20c4820d1b024
Dravid Joseph <dravidjoseph@gmail.com>
2016-03-02 12:48:44 -0600

    Test displaying other content on php page

--- projectideas.php
+++ projectideas.php
@@ -7,12 +7,18 @@ require_once("projectideafunctions.php");
 
 echo '<html>';
 echo '<body>';
-	echo 'Project Ideas';
+	echo '<h1>Project Ideas</h1>';
 	echo '<ul>';
 	echo'<li>'.Get_djosephProjectIdea(0).'</li>';
 	echo'<li>'.Get_djosephProjectIdea(1).'</li>';
 	echo'<li>'.Get_djosephProjectIdea(2).'</li>';
-	echo '<ul>';
+	echo '</ul>';
+	
+	echo '<h2>Michael Wang Ideas</h2>';
+	$mwang = file_get_contents("http://people.eecs.ku.edu/~mwang/projectideafunctions.php?index=0");
+	echo $mwang;
+	
+	
 echo '</body>';
 echo '</html>';
 ?>
